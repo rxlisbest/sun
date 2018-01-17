04d6eb35086d58c02ec60a75dc1bdf80e5a32408
Roy <rxlisbest@gmail.com>
2018-01-17 12:53:39 +0800

    查找控制器

--- src/Application.php
+++ src/Application.php
@@ -7,11 +7,23 @@
  */
 
 namespace Rxlisbest\Sun;
-
+use app\controllers\IndexController;
+use Rxlisbest\Sun\Core\Request;
 
 class Application
 {
-    public function run(){
+    private $config;
 
+    public function __construct($config)
+    {
+        $this->config = $config;
+    }
+
+    public function run()
+    {
+        $base_path = $this->config['base_path'];
+        include $base_path . '/app/controllers/IndexController.php';
+        $controller = new IndexController();
+        $controller->index();
     }
 }

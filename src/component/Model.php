550bb34a728159c7b0578fe35a2f616cf1b76721
Roy <rxlisbest@gmail.com>
2018-09-30 15:09:51 +0800

    生成sql方法

--- src/component/Model.php
+++ src/component/Model.php
@@ -16,24 +16,27 @@ class Model
     protected $db;
 
     private $_table;
+    private $_force;
     private $_field;
     private $_where;
-    private $_offset = 0;
     private $_limit;
     private $_join;
     private $_group;
+    private $_having;
     private $_order;
+    private $_union;
+    private $_lock;
 
-    private $sql_template = 'SELECT %FIELD% FROM %TABLE% %JOIN% %WHERE% %GROUP% %ORDER%';
+    private $select_template = 'SELECT %FIELD% FROM %TABLE% %FORCE% %JOIN% %WHERE% %GROUP% %HAVING% %ORDER% %LIMIT% %UNION% %LOCK%';
 
     public function __construct()
     {
         $db = Sun::createObject($this->db_class, [Sun::$config['database']]);
         $this->db = $db->connection();
-        $this->_table = $this->getTableName();
     }
 
-    public function getTableName(){
+    public function getTableName()
+    {
         $prefix = Sun::$config['database']['prefix'];
         $class_name = get_class($this);
         $class_name = substr($class_name, strrpos($class_name, '\\') + 1);
@@ -44,7 +47,7 @@ class Model
     public static function get()
     {
         return Sun::createObject(get_class());
-//        return $this->db->query('SELECT * FROM migration');
+        //        return $this->db->query('SELECT * FROM migration');
     }
 
     public function field($field)
@@ -53,6 +56,19 @@ class Model
             $field = implode(',', $field);
         }
         $this->_field = $field;
+        return $this;
+    }
+
+    public function force($field)
+    {
+        if (is_array($field)) {
+            $field = implode(',', $field);
+        }
+        if ($field) {
+            $this->_force = " FORCE INDEX (${field})";
+        } else {
+            $this->_force = '';
+        }
         return $this;
     }
 
@@ -108,7 +124,7 @@ class Model
         return implode(" ${condition} ", $where);
     }
 
-    public function limit($offset, $limit)
+    public function limit($offset, $limit = '')
     {
         if (is_string($offset) && strrpos($offset, ',')) {
             list($offset, $limit) = explode(',', $offset);
@@ -116,34 +132,132 @@ class Model
         if (is_array($offset) && count($offset) == 2) {
             list($offset, $limit) = $offset;
         }
+        if (!$limit) {
+            $limit = $offset;
+            $offset = 0;
+        }
 
-        $this->_offset = $offset;
-        $this->_limit = $limit;
+        $this->_limit = " LIMIT ${offset},${limit}";
         return $this;
     }
 
-    public function join($table, $condition)
+    public function join($table, $on, $type = '')
     {
-        $this->_group = $group;
+        if ($type) {
+            $this->_join .= " ${type} JOIN ${table} ON ${on}";
+        } else {
+            $this->_join .= " JOIN ${table} ON ${on}";
+        }
         return $this;
     }
 
     public function group($group)
     {
-        $this->_group = $group;
+        if ($group) {
+            $this->_group = ' GROUP BY ' . $group;
+        } else {
+            $this->_group = '';
+        }
+        return $this;
+    }
+
+    public function having($having)
+    {
+        if ($having) {
+            $this->_having = ' HAVING ' . $having;
+        } else {
+            $this->_having = '';
+        }
         return $this;
     }
 
     public function order($order)
     {
-        $this->_order = $order;
+        if ($order) {
+            $this->_order = ' ORDER BY ' . $order;
+        } else {
+            $this->_order = '';
+        }
         return $this;
     }
 
-    private function sql()
+    public function union($union)
     {
-        if ($this->_field) {
-
+        if ($union) {
+            $this->_union = ' UNION ' . $union;
+        } else {
+            $this->_union = '';
         }
+        return $this;
+    }
+
+    public function lock($lock)
+    {
+        if ($lock) {
+            $this->_lock = ' FOR UPDATE';
+        } else {
+            $this->_lock = '';
+        }
+        return $this;
+    }
+
+    private function parseTable()
+    {
+        $this->_table = ' ' . $this->getTableName();
+    }
+
+    private function parseField()
+    {
+        if (!$this->_field) {
+            $this->_field = " *";
+        }
+    }
+
+    private function parseWhere()
+    {
+        if ($this->_where) {
+            $this->_where = ' WHERE ' . $this->_where;
+        } else {
+            $this->_where = '';
+        }
+        return $this->_where;
+    }
+
+    public function sql()
+    {
+        $this->parseTable();
+        $this->parseField();
+        $this->parseWhere();
+
+        $before = [
+            ' %FIELD%',
+            ' %TABLE%',
+            ' %FORCE%',
+            ' %JOIN%',
+            ' %WHERE%',
+            ' %GROUP%',
+            ' %HAVING%',
+            ' %ORDER%',
+            ' %LIMIT%',
+            ' %UNION%',
+            ' %LOCK%'
+        ];
+        $after = [
+            $this->_field,
+            $this->_table,
+            $this->_force,
+            $this->_join,
+            $this->_where,
+            $this->_group,
+            $this->_having,
+            $this->_order,
+            $this->_limit,
+            $this->_union,
+            $this->_lock,
+        ];
+
+        $this->select_template;
+        $sql = str_replace($before, $after, $this->select_template);
+        return $sql;
     }
 }
